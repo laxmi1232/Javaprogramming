5da7e28d2f7bc310bd2bae4e9feb18a8ac18e949
laxmi1232 <103212174+laxmi1232@users.noreply.github.com>
2022-04-12 20:59:47 +0530

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+     cout<<"Hello guys"<<endl;
+     cout<<"hey i am laxmi sharma "<<endl;
+     cout<<"i am student of mca first year"<<endl;
+
+     return 0;
+}
