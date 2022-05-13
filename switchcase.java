1223a820bf32043d5075e599efba9cdaf1a394cd
laxmi1232 <103212174+laxmi1232@users.noreply.github.com>
2022-05-13 23:36:27 +0530

    Create switchcase.java

---
+++ switchcase.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Main {
+     
+     public static void main(String args[]){
+     
+     Scanner sc = new Scanner(System.in);
+     
+     int  button = sc.nextInt();
+     
+     switch(button){
+         case 1 : System.out.println("hello");
+         break;
+         
+         case 2 : System.out.println("namaste");
+         break;
+         
+         case 3 : System.out.println("gud  morng");
+             break;
+             default : System.out.println("Invalid button");
+             
+     }
+     
+    
+
+     }
+}
+     
+     
+     
+     
