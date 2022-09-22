2940ef629354028672390f7d1fb254b72ba0155e
laxmi1232 <103212174+laxmi1232@users.noreply.github.com>
2022-09-22 06:03:14 +0530

    Create Variable.java

---
+++ Variable.java
@@ -0,0 +1,19 @@
+class Variables {
+    public static void main(String[] args) {
+        
+        int i = 10;
+        int j = 20;
+        
+        short s = 123;
+         long l = 12345;
+         
+         double  d = 1.23;
+         
+         char c = 'a';
+         
+         boolean b = true;
+         boolean B = false;
+        
+        System.out.println(b);
+    }
+}
