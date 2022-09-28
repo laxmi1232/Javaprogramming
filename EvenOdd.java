63b775a6d28f11c5418b3a1e65743269d0e574d7
laxmi1232 <103212174+laxmi1232@users.noreply.github.com>
2022-09-28 18:34:02 +0530

    Create EvenOdd.java

---
+++ EvenOdd.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+
+public class Main
+{
+	public static void main(String[] args){
+	     
+	     int n;
+	     System.out.println("Enter any  Number");
+	     Scanner r = new Scanner(System.in);
+	     n=r.nextInt();
+	     if(n%2 == 0)
+	     {
+	         System.out.println("Even Number ");
+	        
+	     }
+	     else
+	     {
+	        System.out.println("odd Number");
+	     }
+	}
+}
